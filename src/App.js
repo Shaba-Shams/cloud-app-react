edd1969e1b8a2024226603505f7a9a8bf065f21c
Shaba-Shams <shabashams@gmail.com>
2023-01-30 13:31:28 +0600

    commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import NavBar from './components/NavBar'
+import Hero from './components/Hero'
+import About from './components/About';
+import Support from './components/Support';
+import AllInOne from './components/AllInOne';
+import Pricing from './components/Pricing';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <About />
+      <Support />
+      <AllInOne />
+      <Pricing />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
